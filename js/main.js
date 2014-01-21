b87888f48d2e00aa5d1a2cc9f893ea2d33ff4420
Coty Beasley <design@beasleycreative.com>
2014-01-21 13:50:42 -0600

    Added components from previous build, plus a news ticker.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,39 @@
+/*!
+ *
+ *	SOLAR Interaction and Functionality
+ * 	Initially created by Coty Beasley @beacrea
+ *	Email: cdbeasley@naic.org
+ * 
+ * -------------------------------------------
+ * 
+ *  TABLE OF CONTENTS
+ *
+ * -------------------------------------------
+ *
+ *	$_CONTENT COMPONENTS
+ *		- News Ticker
+ *
+ */
+
+
+
+
+
+/*
+ * -------------------------------------------
+ *	$_CONTENT COMPONENT
+ * -------------------------------------------
+ */
+
+// News Ticker
+
+var ticker = $('.news-articleWrap');
+ticker.children(':first').show().siblings().hide();
+
+setInterval(function() {
+    ticker.find(':visible').fadeOut(1000, function() {
+        $(this).appendTo(ticker);
+        ticker.children(':first').fadeIn(1000);
+    });
+},5000);
 
