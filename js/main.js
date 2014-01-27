93ce81ac1ac831c853a9c548bd0d6f42c793b349
Coty Beasley <design@beasleycreative.com>
2014-01-27 10:16:24 -0600

    REDO: Added a tweak to the sbs service nav so it didn't have href attributes that moved the page on click.

--- js/main.js
+++ js/main.js
@@ -100,7 +100,8 @@ $('a, .btn').click(function (event) {
     } 
 });
 
-$('.panelbar a').not('.nav-category > a').addClass('triggerView-reports').removeAttr("href").css('cursor', 'pointer');
+$('.panelbar a').not('.nav-category > a').addClass('triggerView-reports');
+$('.panelbar a').removeAttr("href").css('cursor', 'pointer');
 
 // SBS Service Naviation
 
